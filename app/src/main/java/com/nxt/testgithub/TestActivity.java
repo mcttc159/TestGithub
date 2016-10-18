e65c0e50292fecb89e0a37a8fca475051fc095cd
Nguyen Xuan Thuong <nxuanthuong94@gmail.com>
2016-10-18 08:43:05 +0700

    second test

---
+++ app/src/main/java/com/nxt/testgithub/TestActivity.java
@@ -0,0 +1,12 @@
+package com.nxt.testgithub;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by NXT on 18/10/2016.
+ */
+
+public class TestActivity extends AppCompatActivity{
+
+
+}
