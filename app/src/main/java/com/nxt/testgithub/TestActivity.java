d8188793346b9c4aa0dd114646f41f4a73f42fab
Nguyen Xuan Thuong <nxuanthuong94@gmail.com>
2016-10-18 08:45:19 +0700

    3

--- app/src/main/java/com/nxt/testgithub/TestActivity.java
+++ app/src/main/java/com/nxt/testgithub/TestActivity.java
@@ -1,5 +1,7 @@
 package com.nxt.testgithub;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -7,6 +9,9 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public class TestActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-
+    }
 }
